84def79f01e5920f5b4439548551ad9e5efe3445
Simon Perneel <Simon.Perneel@UGent.be>
2021-11-02 16:39:10 +0100

    Update dash_test.py

--- mobiledna/simon/dash_test.py
+++ mobiledna/simon/dash_test.py
@@ -16,25 +16,43 @@ ae = Appevents.load_data('data_qa.csv', sep=';')
 df = ae.__data__
 
 ids = df.id.unique()
+categories = df.category.unique()
 
 # ------------------------------------------------------------------------------
 # App layout
-app.layout = html.Div([
+app.layout = html.Div(children=[
+    html.Div([
 
-    html.H1("Screen time distribution per category", style={'text-align': 'center'}),
+        html.H1("Screen time distribution per category", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_id",
-                 options=[{"label": x, "value": x} for x in ids],
-                 multi=False,
-                 value=ids[0],
-                 style={'width': "40%"}
-                 ),
-    html.Div(),
-    html.Div(id='output_container', children=[]),
-    html.Br(),
-    dcc.Graph(id='my_mobileDNA_map', figure={}),
+        dcc.Dropdown(id="slct_id",
+                     options=[{"label": x, "value": x} for x in ids],
+                     multi=False,
+                     value=ids[0],
+                     style={'width': "40%"}
+                     ),
+        html.Div(),
+        html.Div(id='output_container', children=[]),
+        html.Br(),
+        dcc.Graph(id='my_mobileDNA_map', figure={}),
+        ]),
 
-    html.H1("", style={'text-align': 'center'}),
+    html.Div([
+
+        html.H1("Category usage over time", style={'text-align': 'center'}),
+
+        dcc.Dropdown(id="slct_category",
+                     options=[{"label": x, "value": x} for x in categories],
+                     multi=False,
+                     value=categories[2],
+                     style={'width': "40%"}
+                     ),
+        html.Div(),
+        html.Div(id='output_container_2', children=[]),
+        html.Br(),
+        dcc.Graph(id='my_mobileDNA_map_2', figure={}),
+
+    ]),
 
 ])
 
@@ -66,28 +84,45 @@ def update_graph(option_slctd):
         data_frame=dff,
         values='duration',
         names='category',
-        title='screen time per category'
+        title='screen time per category',
         #template='plotly_dark'
     )
     fig.update_traces(textposition='inside', textinfo='percent+label')
 
-    # Plotly Graph Objects (GO)
-    # fig = go.Figure(
-    #     data=[go.Choropleth(
-    #         locationmode='USA-states',
-    #         locations=dff['state_code'],
-    #         z=dff["Pct of Colonies Impacted"].astype(float),
-    #         colorscale='Reds',
-    #     )]
-    # )
-    #
-    # fig.update_layout(
-    #     title_text="Bees Affected by Mites in the USA",
-    #     title_xanchor="center",
-    #     title_font=dict(size=24),
-    #     title_x=0.5,
-    #     geo=dict(scope='usa'),
-    # )
+    return container, fig
+
+@app.callback(
+    [Output(component_id='output_container_2', component_property='children'),
+     Output(component_id='my_mobileDNA_map_2', component_property='figure')],
+    [Input(component_id='slct_category', component_property='value')]
+)
+
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "usage in '{}' category for user with id: {}".format(option_slctd, '5e37a125-d7e6-49a1-9f62-b3657f08cdff')
+
+    dff = df.copy()
+    dff['duration'] = (dff['duration'] / 60)
+    dff = dff[dff["category"] == option_slctd]
+    dff = dff[dff["id"] == '5e37a125-d7e6-49a1-9f62-b3657f08cdff']
+
+    dff = dff.groupby(['startDate', 'category'])['duration'].agg(duration='sum')
+    dff.reset_index(inplace=True)
+    print(dff.head())
+
+    # Plotly Express
+    fig = px.bar(
+        data_frame=dff,
+        x='startDate',
+        y='duration',
+        color='duration',
+        color_continuous_scale='greens',
+        # hover_data=['State', 'Pct of Colonies Impacted'],
+        labels={'duration': 'duration (min)', 'startDate': 'date'},
+        #template='plotly_dark'
+    )
 
     return container, fig
 
