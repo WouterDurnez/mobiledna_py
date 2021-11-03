cce695389f5935d796727349663986084f0899af
Simon Perneel <Simon.Perneel@UGent.be>
2021-11-03 10:03:46 +0100

    dashboarding tryouts 3
    3 graphs one one page

--- mobiledna/simon/dash_test.py
+++ mobiledna/simon/dash_test.py
@@ -33,8 +33,21 @@ app.layout = html.Div(children=[
                      ),
         html.Div(),
         html.Div(id='output_container', children=[]),
+
         html.Br(),
-        dcc.Graph(id='my_mobileDNA_map', figure={}),
+
+        dcc.Dropdown(id="slct_id_2",
+                     options=[{"label": x, "value": x} for x in ids],
+                     multi=False,
+                     value=ids[0],
+                     style={'width': "40%"}
+                     ),
+        html.Div(),
+
+
+        html.Div(id='output_container_3', children=[]),
+        dcc.Graph(id='my_mobileDNA_map', figure={}, style={'display': 'inline-block'}),
+        dcc.Graph(id='my_mobileDNA_map_3', figure={}, style={'display': 'inline-block'}),
         ]),
 
     html.Div([
@@ -92,14 +105,40 @@ def update_graph(option_slctd):
     return container, fig
 
 @app.callback(
+    [Output(component_id='output_container_3', component_property='children'),
+     Output(component_id='my_mobileDNA_map_3', component_property='figure')],
+    [Input(component_id='slct_id_2', component_property='value')]
+)
+def update_graph(option_slctd):
+
+    container = "usage for user with id: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff['duration'] = (dff['duration'] / 60)
+    dff = dff[dff["id"] == option_slctd]
+
+    dff = dff.groupby(['category'])['duration'].agg(duration='sum')
+    dff.reset_index(inplace=True)
+
+    # Plotly Express
+    fig = px.pie(
+        data_frame=dff,
+        values='duration',
+        names='category',
+        title='screen time per category',
+        #template='plotly_dark'
+    )
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    return container, fig
+
+@app.callback(
     [Output(component_id='output_container_2', component_property='children'),
      Output(component_id='my_mobileDNA_map_2', component_property='figure')],
     [Input(component_id='slct_category', component_property='value')]
 )
 
 def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
 
     container = "usage in '{}' category for user with id: {}".format(option_slctd, '5e37a125-d7e6-49a1-9f62-b3657f08cdff')
 
@@ -110,7 +149,6 @@ def update_graph(option_slctd):
 
     dff = dff.groupby(['startDate', 'category'])['duration'].agg(duration='sum')
     dff.reset_index(inplace=True)
-    print(dff.head())
 
     # Plotly Express
     fig = px.bar(
